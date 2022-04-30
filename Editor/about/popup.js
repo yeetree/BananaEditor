e79c3fcdae9f4e80f21c5b81f6db765ef0af0935
yeetree <64941543+yeetree@users.noreply.github.com>
2022-04-30 13:44:49 -0600

    files

--- Editor/about/popup.js
+++ Editor/about/popup.js
@@ -1,3 +1,10 @@
 function aboutWindow() {
-    window = window.open("./about/about.html",'popUpWindow','height=420,width=350,left=100,top=100,resizable=no,scrollbars=yes,toolbar=no,menubar=no,location=no,directories=no, status=yes');
-}   
+    window = window.open("./about/about.html",'About Banana Editor','height=420,width=350,left=100,top=100,resizable=no,scrollbars=yes,toolbar=no,menubar=no,location=no,directories=no, status=yes');
+}
+function issueWindow() {
+    issuewindow = window.open("https://github.com/yeetree/BananaEditor/issues/new",'Report an Issue','height=500,width=500,left=100,top=100,resizable=no,scrollbars=yes,toolbar=no,menubar=no,location=no,directories=no, status=yes');
+}
+
+function docsWindow() {
+    docswindow = window.open("./about/docs.html",'Documentation','height=500,width=500,left=100,top=100,resizable=no,scrollbars=yes,toolbar=no,menubar=no,location=no,directories=no, status=yes');
+}  
---
+++ Editor/js/editor.js
@@ -0,0 +1,6 @@
+const body = ["../Sprites/body/banana_0.png", "../Sprites/body/banana_1.png", "../Sprites/body/banana_2.png", "../Sprites/body/banana_3.png"]
+const shirts = ["../Sprites/shirts/shirts_0.png", "../Sprites/shirts/shirts_1.png", "../Sprites/shirts/shirts_2.png", "../Sprites/shirts/shirts_3.png", "../Sprites/shirts/shirts_4.png", "../Sprites/shirts/shirts_5.png"]
+const pants = ["../Sprites/pants/pants_0.png", "../Sprites/pants/pants_1.png", "../Sprites/pants/pants_2.png"]
+
+let app = new PIXI.Application({ width: 800, height: 600 });
+document.body.appendChild(app.view);
