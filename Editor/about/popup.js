b90d19a88b6136de51b3c0f3cc45f3761992f10a
yeetree <64941543+yeetree@users.noreply.github.com>
2022-04-30 13:08:19 -0600

    files

---
+++ Editor/about/popup.js
@@ -0,0 +1,3 @@
+function aboutWindow() {
+    window = window.open("./about/about.html",'popUpWindow','height=420,width=350,left=100,top=100,resizable=no,scrollbars=yes,toolbar=no,menubar=no,location=no,directories=no, status=yes');
+}   
